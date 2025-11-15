60267819747ead3a8c23b202b303cecb16d41994
Joshua Dean Shields <shields.joshua23@gmail.com>
2025-11-15 10:30:00 +0200

    Set up UI theme and styling

---
+++ app/src/main/java/com/example/manutdapp/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.manutdapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val UtdRed = Color(0xFFDA291C)
+val UtdDarkRed = Color(0xFFB61F17)
+val UtdGold = Color(0xFFFBE122)
+val UtdBlack = Color(0xFF1A1A1A)
+val White = Color(0xFFFFFFFF)
+val LightGray = Color(0xFFF5F5F5)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
